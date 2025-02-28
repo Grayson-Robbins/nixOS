13f5b195b7ea9ccb59ff67802647b1e1c5338bb7
gar0018 <gar0018@uah.edu>
2025-02-28 00:13:35 -0600

    Added superfile, hledger, and nix-inspect.

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, nix-colors, ... }:
+{ config, lib, pkgs, nix-colors, inputs, ... }:
 
 {
   home.username = "graysonr"; 
@@ -21,6 +21,9 @@
   home.packages = with pkgs; [
     gotop # Resource monitor TUI
     cbonsai # Nice bonsai tree written in C
+    superfile # Fancy file manager
+    hledger # Plaintext accounting TUI
+    nix-inspect 
   ];
   home.stateVersion = "24.05"; # Ensure compatibility
   programs.home-manager.enable = true;
