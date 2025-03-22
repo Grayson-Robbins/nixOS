1afebc4fda9af72cb92459fa60dde0af2adaa039
gar0018 <gar0018@uah.edu>
2025-03-22 01:06:45 -0500

    Passed NVF flake to overall flake (FIRST TIME, BIG STEP :D)

--- flake.nix
+++ flake.nix
@@ -10,28 +10,22 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    configuredNvim.url = "path:/home/graysonr/nixOS/modules/nixos/nvf";
     stylix.url = "github:danth/stylix";
-    nvf.url = "github:notashelf/nvf"; # NVF flake input
   };
 
-  outputs = { self, nixpkgs, home-manager, nvf, stylix, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, stylix, configuredNvim, ... }@inputs:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    packages.${system}.default = 
-      (nvf.lib.neovimConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [ ./modules/nixos/nvf-configuration.nix ];
-      }).neovim;
+    packages.${system}.default = configuredNvim.packages.${system}.default;
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
-        nvf.nixosModules.default
       ];
     };
 
